deb15697bed162217c37972827ab3494e3b352b6
Tanya Kapriyan <tanya.kapriyan@here.com>
2015-06-01 15:14:00 -0500

    Extracted class for session management

--- test_addnewcontact.py
+++ test_addnewcontact.py
@@ -11,12 +11,12 @@ def app(request):
 
 
 def test_add_new_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_new_contact(Contact(first_name="First", last_name="Last", address="Address", home_phone="12345678", mobile_phone="987654321", email="first.last@test.com"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_new_contact(Contact(first_name="", last_name="", address="", home_phone="", mobile_phone="", email=""))
-    app.logout()
+    app.session.logout()
