4ea5af58f5febba805c1868abbf6aa79964f2b83
Tanya Kapriyan <tkapriyan@gmail.com>
2015-07-09 00:27:26 -0500

    Task 7: Added all  tests for contacts and groups creation, deletion and modification.

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,5 +1,3 @@
-
-
 class ContactHelper:
 
     def __init__(self, app):
@@ -36,6 +34,32 @@ class ContactHelper:
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         # Return to home page
         self.return_to_home_page()
+
+    def modify_first_contact(self, contact):
+        wd = self.app.wd
+        # Initiate contact modification
+        wd.find_element_by_xpath(" //*[@id='maintable']//tr[5]/td[8]/a").click()
+        # Modify contact information
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home_phone)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile_phone)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        # Submit modifications
+        wd.find_element_by_name("update").click()
 
     def delete_first_contact(self):
         wd = self.app.wd
---
+++ test/test_modify_contact.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+
+def test_modify_first_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.modify_first_contact(Contact(first_name="New_name", last_name="New_Last_name", address="New_Address", home_phone="11111111", mobile_phone="111111111", email="new.first.last@test.com"))
+    app.session.logout()
