6f764dfa91dee3ea7ef51a62e353072d8a8d86e5
Tanya Kapriyan <tkapriyan@gmail.com>
2015-07-11 14:02:58 -0500

    Added comparison of lists for deletion and modification of groups

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -4,4 +4,7 @@ from model.group import Group
 def test_delete_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Added group"))
+    old_groups = app.group.get_groups_list()
     app.group.delete_first_group()
+    new_groups = app.group.get_groups_list()
+    assert len(old_groups) - 1 == len(new_groups)
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,16 +4,26 @@ from model.group import Group
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Added group"))
+    old_groups = app.group.get_groups_list()
     app.group.modify_first_group(Group(name="Updated name"))
+    new_groups = app.group.get_groups_list()
+    assert len(old_groups) == len(new_groups)
+
 
 
 def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Added group"))
+    old_groups = app.group.get_groups_list()
     app.group.modify_first_group(Group(header="Updated header"))
+    new_groups = app.group.get_groups_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_footer(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Added group"))
+    old_groups = app.group.get_groups_list()
     app.group.modify_first_group(Group(footer="Updated footer"))
+    new_groups = app.group.get_groups_list()
+    assert len(old_groups) == len(new_groups)
