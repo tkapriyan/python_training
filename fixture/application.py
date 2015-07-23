982ca4bf0553fd1cdb8ad9d2fe735678f487567c
Tanya Kapriyan <tkapriyan@gmail.com>
2015-07-22 23:51:57 -0500

    Task 16: Implemented command line options for browser and base url

--- fixture/application.py
+++ fixture/application.py
@@ -1,22 +1,24 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
 
 
 class Application:
-    def __init__(self):
-        self.wd = WebDriver()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
-
-    def destroy(self):
-        self.wd.quit()
+        self.browser = browser
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -24,3 +26,10 @@ class Application:
             return True
         except:
             return False
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get(self.base_url)
+
+    def destroy(self):
+        self.wd.quit()
