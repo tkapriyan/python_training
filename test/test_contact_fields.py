7b919220a4739bc800b34861554a224fec36a131
Tanya Kapriyan <tkapriyan@gmail.com>
2015-07-27 23:18:27 -0500

    Task 21: Comparing contacts data fetched from UI to data fetched from database

--- test/test_contact_fields.py
+++ test/test_contact_fields.py
@@ -4,10 +4,12 @@ from fixture.contact import Contact
 
 
 def test_home_page_fields(app, db):
+    if len(db.get_contacts_list()) == 0:
+        app.contact.create(Contact(first_name="First", last_name="Last", address="Address", home_phone="12345678",
+                                   work_phone="(312)98756-32", secondary_phone="312-987-78-23",
+                                   email="first.last@test.com", email2="second.email@test.com"))
     home_page_contacts_list = map(format_entry, app.contact.get_contacts_list())
-    # home_page_contacts_list = map(format_entry, app.contact.get_contacts_list())
     db_contacts_list = map(format_entry, db.get_contacts_list())
-    # db_contacts_list = map(format_entry, db.get_contacts_list())
     assert sorted(home_page_contacts_list, key=Contact.id_or_max) == sorted(db_contacts_list, key=Contact.id_or_max)
 
 
@@ -29,8 +31,9 @@ def merge_emails_like_on_home_page(contact):
                                 filter(lambda x: x is not None,
                                        [contact.email, contact.email2, contact.email3]))))
 
+
 def format_entry(contact):
-        return Contact(contact_id=contact.contact_id, first_name=contact.first_name.strip(),
-                       last_name=contact.last_name.strip(),
-                       all_phones_from_home_page=merge_phones_like_on_home_page(contact),
-                       all_emails_from_home_page=merge_emails_like_on_home_page(contact))
+    return Contact(contact_id=contact.contact_id, first_name=clear(contact.first_name.strip()),
+                   last_name=clear(contact.last_name.strip()),
+                   all_phones_from_home_page=merge_phones_like_on_home_page(contact),
+                   all_emails_from_home_page=merge_emails_like_on_home_page(contact))
